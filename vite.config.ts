022bebfbf56f3d9f2e35b36fee5de63380780a29
Abhay <vboxuser48@gmail.com>
2025-07-25 18:24:08 +0530

    add base path for Github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/the-comedy-theatre/', // 👈 required for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
